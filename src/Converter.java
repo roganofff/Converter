2fc566b13b23773789b1ae97ba749457e546869e
roganoff <matematic1337@gmail.com>
2022-10-27 11:14:11 +0300

    ver 0.1

---
+++ src/Converter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Converter {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scan.nextInt();
+
+        System.out.println("Enter a number system of number: ");
+        int numSystem = scan.nextInt();
+
+        System.out.println("Enter a desired number system: ");
+        int desNumSystem = scan.nextInt();
+    }
+}
